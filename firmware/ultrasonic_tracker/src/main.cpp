0f497032c55a086f0710ad4083a87d15df153587
melektron <matteo@elektron.work>
2023-10-25 15:07:49 +0200

    attempted to add ultrasound stuff

--- firmware/ultrasonic_tracker/src/main.cpp
+++ firmware/ultrasonic_tracker/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-#define PIN_ECHO 26
-#define PIN_TRIG 27
+#define PIN_ECHO 17
+#define PIN_TRIG 16
 
 void setup()
 {
@@ -10,9 +10,26 @@ void setup()
 
 
     pinMode(PIN_TRIG, OUTPUT);
-    pinMode(PIN_ECHO, INPUT);
+    pinMode(PIN_ECHO, INPUT_PULLUP);
 }
+
+double last_distance = 0;
 
 void loop()
 {
+    digitalWrite(PIN_TRIG, HIGH); 
+    delayMicroseconds(10); 
+    //delay(500);
+    digitalWrite(PIN_TRIG, LOW); 
+
+    int duration = pulseIn(PIN_ECHO, HIGH, 8000);
+
+    printf("duration=%d\n", duration);
+
+    last_distance = duration * 0.0344 / 2;
+    //last_distance = (last_distance / max_distance) * 100;
+
+    printf("distance: %lf\n", last_distance);
+    
+    delay(300);
 }
